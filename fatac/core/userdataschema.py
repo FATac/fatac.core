ac046b3d121aa2faea1c57b4c7af411ecae1b260
Laura Pérez Mayos <lpmayos@gmail.com>
2012-08-03 10:53:56 +0200

    canvi perque gaci servir fatac.theme.po

--- fatac/core/userdataschema.py
+++ fatac/core/userdataschema.py
@@ -1,8 +1,7 @@
 from zope.interface import implements
 from zope import schema
 
-from Products.CMFPlone import PloneMessageFactory as _
-from fatac.theme import FatacThemeMessageFactory as FATACMF
+from fatac.theme import FatacThemeMessageFactory as _
 from plone.app.users.userdataschema import IUserDataSchemaProvider
 from plone.app.users.userdataschema import IUserDataSchema
 from plone.app.users.userdataschema import checkEmailAddress
@@ -30,48 +29,48 @@ class IEnhancedUserDataSchema(IUserDataSchema):
     extra fields.
     """
     fullname = schema.TextLine(
-        title=FATACMF(u'label_full_name', default=u'Full Name'),
-        description=FATACMF(u'help_full_name_creation',
+        title=_(u'label_full_name', default=u'Full Name'),
+        description=_(u'help_full_name_creation',
                       default=u"Enter full name, e.g. John Smith."),
         required=True)
 
     email = schema.ASCIILine(
-        title=FATACMF(u'email', default=u'E-mail'),
+        title=_(u'email', default=u'E-mail'),
         description=u'',
         required=True,
         constraint=checkEmailAddress)
 
     occupation = schema.TextLine(
-        title=FATACMF(u'label_occupation', default=u'Occupation'),
-        description=FATACMF(u'help_city',
+        title=_(u'label_occupation', default=u'Occupation'),
+        description=_(u'help_city',
                       default=u"Fill in your occupation or the work you develop."),
         required=True,
         )
 
     institution = schema.TextLine(
-        title=FATACMF(u'label_institution', default=u'Institution'),
-        description=FATACMF(u'help_city',
+        title=_(u'label_institution', default=u'Institution'),
+        description=_(u'help_city',
                       default=u"Fill in the institution you represent or work for."),
         required=False,
         )
 
     city = schema.TextLine(
-        title=FATACMF(u'label_city', default=u'City'),
-        description=FATACMF(u'help_city',
+        title=_(u'label_city', default=u'City'),
+        description=_(u'help_city',
                       default=u"Fill in the city you live in."),
         required=True,
         )
 
     home_page = schema.TextLine(
-        title=FATACMF(u'label_homepage', default=u'Home page'),
-        description=FATACMF(u'help_homepage',
+        title=_(u'label_homepage', default=u'Home page'),
+        description=_(u'help_homepage',
                       default=u"The URL for your external home page, "
                       "if you have one."),
         required=False)
 
     accept = schema.Bool(
-        title=FATACMF(u'label_accept', default=u'Accept terms of use'),
-        description=FATACMF(u'help_accept',
+        title=_(u'label_accept', default=u'Accept terms of use'),
+        description=_(u'help_accept',
                       default=u"Tick this box to indicate that you have found,"
                       " read and accepted the terms of use for this site. "),
         required=True,
